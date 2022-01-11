1a224a46293c40d8c6a84eb1ad192ebcb57d7487
TT84 <tthompson8416@outlook.com>
2022-01-11 16:18:02 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,7 +83,7 @@ console.log(promptFight);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyNames + " now has " + enemyHealth + " health remaining. " 
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. " 
     );
 };
 
@@ -102,7 +102,7 @@ console.log(promptFight);
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
         // pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames;
+        var pickedEnemyName = enemyNames[i];
         
         // reset enemyHealth before starting new fight
         enemyHealth = 50;
@@ -119,16 +119,34 @@ console.log(promptFight);
         break;
       }
     }
-    // play again
-    startGame();
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
 
     };
+    // after loop ends, we are either out of playerHealth or enemies to fight, so run the endGame function
+    var endGame = function() {
+        // if player is still alive, player wins!
+        if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+     }
 
-      // after loop ends, we are either out of playerHealth or enemies to fight, so run the endGame function
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+     };
+    }   
+
   
-
-
-    // start first game when page loads
+    // call startGame: start first game when page loads
     startGame();
 
     
