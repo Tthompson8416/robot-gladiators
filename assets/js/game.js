717c676a70b54d4c1eda7a2e588e3c3f48d3e028
TT84 <tthompson8416@outlook.com>
2022-01-17 15:15:07 -0500

    bug/player-name branch added

--- assets/js/game.js
+++ assets/js/game.js
@@ -116,6 +116,8 @@ var startGame = function () {
         if (playerInfo.health > 0) {
             // let player know what round they are in, remeber that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            debugger;
+
 
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyObj = enemyInfo[i]; 
@@ -230,6 +232,20 @@ var startGame = function () {
 
     };
 }
+
+// function to set name
+getPlayerName() {
+var name = "";
+
+// ******************************* 
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+// *******************************
+
+console.log("Your robot's name is " + name);
+  return name;
+};
+
+
 
 var playerInfo = {
     name: window.prompt("What is your robot's name?"),
